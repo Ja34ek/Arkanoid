0d3bd6f5ea5a8840e60fa9b5fe0a53ed2cf450cb
Janek Gryczka <janekgryczka@gmail.com>
2023-05-28 12:51:52 +0200

    Add basic data structures

---
+++ src/Data.hs
@@ -0,0 +1,10 @@
+module Data where
+
+windowWidthFloat :: Float
+windowWidthFloat = 400
+
+windowHeightFloat :: Float
+windowHeightFloat = 600
+
+ballRadius :: Float
+ballRadius = 6
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Graphics.Gloss.Interface.Pure.Game
+
+data Brick = Brick {position :: Point, size :: Point} | NoBrick deriving (Eq, Show)
+data Hit = LeftHit | RightHit | TopHit | BottomHit | NoHit | PlatformHit deriving (Eq, Show)
+data Result = Win | Lose deriving (Eq, Show)
+data View = MainMenu | ResultsMenu | LevelView | StartScreen | Pause | Menu | WinView | LoseView | Exit deriving Eq
+data KeyPressed = LeftPressed | RightPressed | NonePressed deriving Eq
+type KeysPressed = [KeyPressed]
+type BricksGridRow = [Brick]
+data BricksGrid = BricksGrid {bricks :: [BricksGridRow], lastHit :: Hit}
+
+data GameState = GameState {
+  name :: String,
+  isSaved :: Bool,
+  isPlaying :: Bool,
+  view :: View,
+  ballPos :: Point,
+  ballDirection :: Vector,
+  platformPos :: Point,
+  level :: Int,
+  grid :: BricksGrid,
+  bricksLeft :: Int,
+  result :: Result,
+  keysPressed :: KeysPressed
+}
+
